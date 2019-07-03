92fbbf313127d86b813e6a999fe2462dc1963a99
binnykt <binnykt@gmail.com>
2019-07-03 11:27:38 +0530

    Fixed Drupal coding standard violations

--- src/Controller/JsonconverterController.php
+++ src/Controller/JsonconverterController.php
@@ -2,41 +2,46 @@
 
 namespace Drupal\siteinfo_modding\Controller;
 
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\Entity\Node;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ *
+ */
 class JsonconverterController extends ControllerBase {
 
   /**
    * {@inheritdoc}
    */
   public function delivernodejson($apikey, $nid) {
-    
-    if(!isset($nid) && !isset($apikey)) {
-       throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException();
+
+    if (!isset($nid) && !isset($apikey)) {
+      throw new AccessDeniedHttpException();
     }
 
-    // Getting the default api key stored in our form
-    $config = \Drupal::config('system.site'); 
+    // Getting the default api key stored in our form.
+    $config = \Drupal::config('system.site');
     $default_apikey = $config->get('siteapikey');
 
-    if($apikey == $default_apikey) {
-	  $response = new JsonResponse();
-	  $node = Node::load($nid);
-	  $data = array(
-	    'node' => array(
-	    'title' => $node->get('title')->getValue()[0]['value'],
-	    'body' => $node->get('body')->getValue()[0]['value'],
-	   )
-	  );
+    if ($apikey == $default_apikey) {
+      $response = new JsonResponse();
+      $node = Node::load($nid);
+      $data = [
+        'node' => [
+          'title' => $node->get('title')->getValue()[0]['value'],
+          'body' => $node->get('body')->getValue()[0]['value'],
+        ],
+      ];
 
-	  $response->setData($data);
+      $response->setData($data);
 
       return $response;
     }
     else {
-      throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException();
+      throw new AccessDeniedHttpException();
     }
   }
+
 }
--- src/Form/SiteInformationModifiedForm.php
+++ src/Form/SiteInformationModifiedForm.php
@@ -1,60 +1,63 @@
 <?php
+
 namespace Drupal\siteinfo_modding\Form;
 
 // Classes referenced in this class:
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Form\ConfigFormBase;
 
-// We are extending the site information form
+// We are extending the site information form.
 use Drupal\system\Form\SiteInformationForm;
 
 /**
  * Configure site information settings for this site.
  */
 class SiteInformationModifiedForm extends SiteInformationForm {
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(array $form, FormStateInterface $form_state)    {
-        // Retrieve the system.site configuration
-        $site_config = $this->config('system.site');
 
-        // Get the original form from the class we are extending
-        $form = parent::buildForm($form, $form_state);
-        $siteapikey_value = $site_config->get('siteapikey');
-        // Add a textarea to the site information section of the form for our
-        // description
-        $form['site_apikey'] = [
-          '#type' => 'details',
-          '#title' => t('Site API Key'),
-          '#open' => TRUE,
-        ];
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Retrieve the system.site configuration.
+    $site_config = $this->config('system.site');
 
-        $form['site_apikey']['siteapikey'] = [
-          '#type' => 'textfield',
-          '#title' => t('Site API Key'),
-          // The default value is the new value we added to our configuration
-          // in step 1
-          '#default_value' => isset($siteapikey_value) ? $siteapikey_value : "No API Key Yet",
-          '#description' => $this->t('This the API Key of the site'),
-        ];
-    
-        return $form;
-    }
+    // Get the original form from the class we are extending.
+    $form = parent::buildForm($form, $form_state);
+    $siteapikey_value = $site_config->get('siteapikey');
+    // Add a textarea to the site information section of the form for our
+    // apikey value.
+    $form['site_apikey'] = [
+      '#type' => 'details',
+      '#title' => t('Site API Key'),
+      '#open' => TRUE,
+    ];
 
-    /**
-     * {@inheritdoc}
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state)    {
-       
-        $this->config('system.site')
-           ->set('siteapikey', $form_state->getValue('siteapikey'))
-           ->save();
+    $form['site_apikey']['siteapikey'] = [
+      '#type' => 'textfield',
+      '#title' => t('Site API Key'),
+        // The default value is the new value we added to our configuration
+        // in step 1.
+      '#default_value' => isset($siteapikey_value) ? $siteapikey_value : "No API Key Yet",
+      '#description' => $this->t('This the API Key of the site'),
+    ];
 
-         \Drupal::messenger()->addMessage(t('The form value has been saved'), 'message');  
+    return $form;
+  }
 
-        // Pass the remaining values off to the original form that we have extended,
-        // so that they are also saved
-        parent::submitForm($form, $form_state);
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $this->config('system.site')
+      ->set('siteapikey', $form_state->getValue('siteapikey'))
+      ->save();
+
+    // Trying to set a custom message with api key.
+    \Drupal::messenger()->addMessage(t('The Site API key  value has been saved with the value' . $form_state->getValue('siteapikey')), 'message');
+
+    // Pass the remaining values off to the original form that we have extended,
+    // so that they are also saved.
+    parent::submitForm($form, $form_state);
+  }
+
 }
